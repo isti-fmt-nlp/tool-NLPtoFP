d9ed0cb59441fec5ce5107e30135809687eac15d
Giorgio <isiu@MacBook-Pro.local>
2015-05-15 17:31:02 +0200

    fix example

---
+++ CMT_FDE_Tool/src/main/CMTConstants.java
@@ -0,0 +1,155 @@
+package main;
+
+public class CMTConstants {
+	
+	/** parent of top level save folder*/
+	private static final String topSaveFolderParent = "..";
+
+	/*Linux Versions*/	
+	/** top level save folder for both tools*/
+	private static final String topSaveFolder = topSaveFolderParent+"/CMT_FDE_Workspace";
+
+	/*Windows Versions*/
+	/** top level save folder for both tools*/
+	private static final String topSaveFolder_Win = topSaveFolderParent+"\\CMT_FDE_Workspace";
+
+	/* Save paths for analisys tool*/
+
+	/*Linux Versions*/	
+	/** general save path for analisys tool*/
+	private static final String saveAnalisysDir = topSaveFolder+"/ANALISYS";
+	
+	/** sub path used to save commonalities candidates*/
+	private static final String CCsubpath = "/CommonalitiesC.log";
+	/** sub path used to save commonalities selected*/
+	private static final String CSsubpath = "/CommonalitiesS.log";
+	/** sub path used to save HTML file with the commonalities selected table*/
+	private static final String CSHsubpath = "/CommonalitiesS.html";
+	/** sub path used to save variabilities candidates*/
+	private static final String VCsubpath = "/VariabilitiesC.log";
+	/** sub path used to save variabilities selected*/
+	private static final String VSsubpath = "/VariabilitiesS.log";
+	/** sub path used to save HTML file with the variabilities selected table*/
+	private static final String VSHsubpath = "/VariabilitiesS.html";
+	/** sub path used to save relevant terms occurrences*/
+	private static final String RTsubpath = "/RelevantTerms.log";
+	/** sub path used to save relevant terms versions*/
+	private static final String TVsubpath = "/TermsVersions.log";
+	
+
+	/*Windows Versions*/
+	/** general save path for analisys tool*/
+	private static final String saveAnalisysDir_Win = topSaveFolder_Win+"\\ANALISYS";
+	
+	/** sub path used to save commonalities candidates*/
+	private static final String CCsubpath_Win = "\\CommonalitiesC.log";
+	/** sub path used to save commonalities selected*/
+	private static final String CSsubpath_Win = "\\CommonalitiesS.log";
+	/** sub path used to save HTML file with the commonalities selected table*/
+	private static final String CSHsubpath_Win = "\\CommonalitiesS.html";
+	/** sub path used to save variabilities candidates*/
+	private static final String VCsubpath_Win = "\\VariabilitiesC.log";
+	/** sub path used to save variabilities selected*/
+	private static final String VSsubpath_Win = "\\VariabilitiesS.log";
+	/** sub path used to save HTML file with the variabilities selected table*/
+	private static final String VSHsubpath_Win = "\\VariabilitiesS.html";
+	/** sub path used to save relevant terms occurrences*/
+	private static final String RTsubpath_Win = "\\RelevantTerms.log";
+	/** sub path used to save relevant terms versions*/
+	private static final String TVsubpath_Win = "\\TermsVersions.log";
+	
+	/* Suffixes for analisys tool*/
+	public static final String SETSsuffix = ".sets";
+	public static final String ARITYsuffix = ".arities";
+	public static final String TERMSsuffix = ".terms";
+	
+	
+	/* Save paths for diagram editor tool*/
+
+	/*Linux Versions*/
+	/** general save path for diagram editor tool*/
+	private static final String saveDiagramDir = topSaveFolder+"/DIAGRAMS";
+	
+	/*Windows Versions*/
+	private static final String saveDiagramDir_Win = topSaveFolder_Win+"\\DIAGRAMS";
+
+
+	/** sub path used to save exported SXFM diagram files*/
+	public static String sxfmSubPath = "_SXFM"; 	
+	/** sub path used to save exported PNG diagram files*/
+	public static String imagesSubPath = "_IMAGES"; 
+	/** sub path used to save custom diagram files, without using data from the analisys tool*/
+	public static String customSaveDiagramDir = "_CUSTOM_DIAGRAMS_";
+
+	/** Returns the path of the top save directory's parent directory*/
+	public static String getTopSaveDirParent(){
+	  return topSaveFolderParent;
+	}
+
+	/** Returns the path of the top save directory*/
+	public static String getTopSaveDir(){
+	  if(OSUtils.isWindows()) return topSaveFolder_Win;
+	  else return topSaveFolder;	  
+	}
+
+	/** Returns the general sub path used for analisys tool*/
+	public static String getSaveAnalisysDir(){
+	  if(OSUtils.isWindows()) return saveAnalisysDir_Win;
+	  else return saveAnalisysDir;	  
+	}
+	
+	/** Returns the sub path used to save commonalities candidates*/
+	public static String getCCsubpath(){		
+	  if(OSUtils.isWindows()) return CCsubpath_Win;
+	  else return CCsubpath;
+	}
+
+	/** Returns the sub path used to save commonalities selected*/
+	public static String getCSsubpath(){
+		  if(OSUtils.isWindows()) return CSsubpath_Win;
+		else return CSsubpath;
+	}
+
+	/** Returns the sub path used to save HTML file with the commonalities selected table*/
+	public static String getCSHsubpath(){
+	  if(OSUtils.isWindows()) return CSHsubpath_Win;
+	  else return CSHsubpath;
+	}
+
+	/** Returns the sub path used to save variabilities candidates*/
+	public static String getVCsubpath(){
+	  if(OSUtils.isWindows()) return VCsubpath_Win;
+	  else return VCsubpath;
+	}
+
+	/** Returns the sub path used to save variabilities selected*/
+	public static String getVSsubpath(){
+	  if(OSUtils.isWindows()) return VSsubpath_Win;
+	  else return VSsubpath;
+	}
+
+	/** Returns the sub path used to save HTML file with the variabilities selected table*/
+	public static String getVSHsubpath(){
+	  if(OSUtils.isWindows()) return VSHsubpath_Win;
+	  else return VSHsubpath;
+	}
+
+	/** Returns the sub path used to save relevant terms occurrences*/
+	public static String getRTsubpath(){
+	  if(OSUtils.isWindows()) return RTsubpath_Win;
+	  else return RTsubpath;
+	}
+
+	/** Returns the sub path used to save relevant terms versions*/
+	public static String getTVsubpath(){
+	  if(OSUtils.isWindows()) return TVsubpath_Win;
+	  else return TVsubpath;
+	}
+
+	/** Returns the general sub path used for diagram editor tool*/
+	public static String getSaveDiagramDir(){
+	  if(OSUtils.isWindows()) return saveDiagramDir_Win;
+	  else return saveDiagramDir;
+	}
+
+}
---
+++ CMT_FDE_Tool/src/main/CreateT2kFile.java
@@ -0,0 +1,147 @@
+package main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import cnr.ilc.t2k.*;
+
+public class CreateT2kFile {
+
+	private String NomeFile;
+	private t2kCore t2k;
+	private File file;
+
+	public CreateT2kFile(File f){
+		NomeFile = f.getName();
+		t2k = new t2kCore("","");
+		file = f;
+		t2k.executeNewCorpus(file, file.getName(), Language.English);
+
+	}
+
+	public File run(){
+
+
+		String absolutePath = file.getAbsolutePath();
+		System.out.println("File path : " + absolutePath);
+
+		String filePath = absolutePath.
+				substring(0,absolutePath.lastIndexOf(File.separator));
+
+		String namedir = filePath+"/"+file.getName().
+				substring(0,file.getName().lastIndexOf("."))+"/";
+
+		new File(namedir).mkdirs();
+
+		t2k.executePartOfSpeech();
+
+		while(!t2k.QueryPartofSpeech()){
+			try {
+				Thread.sleep(5000);
+				System.out.println("Pausa POS ");
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		t2k.downloadPartofSpeech(namedir);
+
+		List<Term_Extraction_Configuration> col = t2k.getListTerm_Extraction_Configuration();
+
+		t2k.executeTerm_Extraction(col.get(2));
+
+		while(!t2k.QueryTerm_Extraction()){
+			try {
+				Thread.sleep(5000);
+				System.out.println("Pausa TE ");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		t2k.downloadTerm_Extraction(namedir);
+
+
+		t2k.executeTerm_Extraction_Indexer();
+
+		while(!t2k.QueryTerm_Extraction_Indexer()){
+			try {
+				Thread.sleep(5000);
+				System.out.println("Pausa TEI ");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		t2k.downloadTerm_Extraction_Indexer(namedir);
+
+		t2k.delCorpus();
+		
+		File dir = new File(namedir);
+		if (dir.isDirectory()) { // make sure it's a directory
+		    for (final File f : dir.listFiles()) {
+		        try {
+		            File newfile =new File(f.getAbsolutePath().substring(0, f.getAbsolutePath().lastIndexOf(".")).replace(".txt", ""));
+
+		            if(f.renameTo(newfile)){
+		                System.out.println("Rename succesful");
+		            }else{
+		                System.out.println("Rename failed");
+		            }
+		        } catch (Exception e) {
+		            // TODO: handle exception
+		            e.printStackTrace();
+		        }
+		    }
+		}
+		File f = new File(namedir+file.getName());
+		copyFile(file,f);
+
+		
+		return dir;
+
+
+	}
+
+	private static void copyFile(File sourceFile, File destFile)
+	{
+		try {
+			if (!sourceFile.exists()) {
+				return;
+			}
+			if (!destFile.exists()) {
+
+				destFile.createNewFile();
+
+			}
+			FileChannel source = null;
+			FileChannel destination = null;
+			source = new FileInputStream(sourceFile).getChannel();
+			destination = new FileOutputStream(destFile).getChannel();
+			if (destination != null && source != null) {
+				destination.transferFrom(source, 0, source.size());
+			}
+			if (source != null) {
+				source.close();
+			}
+			if (destination != null) {
+				destination.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+}
